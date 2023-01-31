c9ed7f54bbee4aa0c4422d8c9c7389497138f13b
Umair Ahmadh <umaireravur@gmail.com>
2023-01-31 16:50:41 +0530

    Update fir_filter.sv

--- fir_filter.sv
+++ fir_filter.sv
@@ -1,37 +1,58 @@
-`timescale 1ns/1ns
-
 module fir_filter (
-    input wire clk,
-    input wire reset,
-    input wire update_coeff,
-    input wire [15:0] data_in,
-    input wire [15:0] coeff[40:0],
-    output reg [31:0] data_out
+  input logic clk,
+  input logic reset,
+  input logic coeff_update,
+  input logic [5:0] coeff_sel,
+  input logic [15:0] new_coeff,
+  input logic [15:0] din,
+  output logic [15:0] dout
 );
-    reg [15:0] coeff_reg[40:0];
-    reg [15:0] data_reg[40:0];
-    reg [31:0] acc;
-    reg [3:0] i;
+  logic [15:0] reg_x[41];
+  logic [15:0] reg_y;
+  logic [15:0] coeff[41];
+  integer i;
 
-    always @(posedge clk) begin
-        if (reset) begin
-            for (i = 40; i > 0; i--) begin
-                data_reg[i] <= 0;
-                coeff_reg[i] <= 0;
-            end
-            acc <= 0;
-        end else begin
-            if(update_coeff) begin
-                coeff_reg <= coeff;
-            end else begin
-                data_reg[0] <= data_in;
-                acc <= acc + data_reg[0] * coeff_reg[0];
-                for (i = 39; i > 0; i--) begin
-                    data_reg[i] <= data_reg[i-1];
-                    acc <= acc + data_reg[i] * coeff_reg[i];
-                end
-                data_out <= acc;
-            end
-        end
+  initial begin
+    for (int i = 0; i < 41; i++)
+      coeff[i] = 16'b1;
+  end
+
+  always_ff @(posedge clk) begin
+    if (reset) begin
+      for(int i=0; i<41; i++) begin
+      reg_x[i] <= {16'b0};
+      end
+      reg_y <= 16'b0;
+    end else begin
+      for (i=40; i>0; i=i-1) begin
+        reg_x[i] <= reg_x[i-1];
+      end
+      reg_x[0] <= din;
+      
+//      //reg_y <= ({41{coeff[40:0]}} * reg_x[40:0]);
+//      reg_y<=0;
+//      for(int i=0; i<41; i++) begin
+//        reg_y <= coeff[i] * reg_x[i];
+//      end
+     
     end
+    
+ 
+  end
+  always_comb if (coeff_update) begin
+    coeff[coeff_sel] <= new_coeff;
+    reg_y<=0;
+  end
+always_comb if(!reset) begin
+      //reg_y <= ({41{coeff[40:0]}} * reg_x[40:0]);
+      //reg_y<=0;
+      for(int i=0; i<41; i++) begin
+        reg_y <= coeff[i] * reg_x[i];
+      end
+        
+
+ end
+
+
+  assign dout = reg_y;
 endmodule
